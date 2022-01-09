2c1fddefbbe479982a0c6c278598cf2ef7d4d6f6
SCARLETT SWERDLOW <scarlettswerdlow@SCARLETTs-MacBook.local>
2022-01-09 09:57:59 -0500

    Added config file

--- get_data.py
+++ get_data.py
@@ -8,10 +8,6 @@ import pandas as pd
 #                                                                              #
 ################################################################################
 
-AUTH_URL = 'https://accounts.spotify.com/api/token'
-
-BASE_URL = 'https://api.spotify.com/v1/'
-
 TSWIFT_ALBUM_IDS = ['7mzrIsaAjnXihW3InKjlC3','2dqn5yOQWdyGwOpOIi9O4x',
                     '5MfAxS5zz8MlfROjGQVXhy','1EoDsNmgTLtmwe1BDAVxV5',
                     '2QJmrSgbdM35R67eoGQo4j','6DEjYFkNZh67HP7R9PSZvv',
@@ -29,10 +25,10 @@ def read_yaml(file_path):
     with open(file_path, "r") as f:
         return yaml.safe_load(f)
 
-def get_access_token(client_id, client_secret):
+def get_access_token(auth_url, client_id, client_secret):
 
     auth_response = requests.post(
-        AUTH_URL,
+        auth_url,
         {'grant_type': 'client_credentials',
         'client_id': client_id,
         'client_secret': client_secret
@@ -51,10 +47,10 @@ def make_headers(access_token):
 
     return headers
 
-def get_album_data(album_id, headers):
+def get_album_data(base_url, album_id, headers):
 
     r = requests.get(
-        BASE_URL + 'albums/' + album_id,
+        base_url + 'albums/' + album_id,
         headers = headers
     )
 
@@ -68,10 +64,10 @@ def get_album_data(album_id, headers):
 
     return album_data
 
-def get_album_tracks(album_id, headers):
+def get_album_tracks(base_url, album_id, headers):
 
     r = requests.get(
-        BASE_URL + 'albums/' + album_id + '/tracks',
+        base_url + 'albums/' + album_id + '/tracks',
         headers = headers
     )
         
@@ -81,17 +77,17 @@ def get_album_tracks(album_id, headers):
 
     return track_ids
 
-def get_track_data(track_id, headers):
+def get_track_data(base_url, track_id, headers):
 
     r_track = requests.get(
-        BASE_URL + 'tracks/' + track_id,
+        base_url + 'tracks/' + track_id,
         headers = headers
     )
     
     d_track = r_track.json()
 
     r_features = requests.get(
-        BASE_URL + 'audio-features/' + track_id,
+        base_url + 'audio-features/' + track_id,
         headers = headers
     )
 
@@ -110,7 +106,7 @@ def get_track_data(track_id, headers):
 
     return track_data
 
-def build_dfs(album_ids, headers, verbose):
+def build_dfs(album_ids, base_url, headers, verbose):
 
     albums_data = []
 
@@ -120,7 +116,7 @@ def build_dfs(album_ids, headers, verbose):
 
         if verbose: print('Getting album ', album_id)
 
-        album_data = get_album_data(album_id, headers)
+        album_data = get_album_data(base_url, album_id, headers)
 
         if verbose: print('Got ', album_data['album_name'])
 
@@ -128,11 +124,11 @@ def build_dfs(album_ids, headers, verbose):
 
         if verbose: print('Getting tracks')
 
-        track_ids = get_album_tracks(album_id, headers)
+        track_ids = get_album_tracks(base_url, album_id, headers)
 
         for track_id in track_ids:
             
-            track_data = get_track_data(track_id, headers)
+            track_data = get_track_data(base_url, track_id, headers)
 
             track_data['album_name'] = album_data['album_name']
             
@@ -151,13 +147,19 @@ def main(config_fp, album_fp, tracks_fp, verbose):
     config = read_yaml(config_fp)
 
     access_token = get_access_token(
+        config['SPOTIFY']['AUTH_URL'],
         config['SPOTIFY']['CLIENT_ID'], 
         config['SPOTIFY']['CLIENT_SECRET']
     )
 
     headers = make_headers(access_token)
 
-    album_df, track_df = build_dfs(TSWIFT_ALBUM_IDS, headers, verbose)
+    album_df, track_df = build_dfs(
+        TSWIFT_ALBUM_IDS, 
+        config['SPOTIFY']['BASE_URL'],
+        headers, 
+        verbose
+    )
 
     album_df.to_csv(album_fp, index = False)
 
